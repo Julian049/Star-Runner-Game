42590e8387b8ee990c9bdd59ca08ae2a3ef16827
Julian <julianb0904@gmail.com>
2025-08-30 22:08:25 -0500

    Created coinCount to manage score

--- src/model/ScoreManager.js
+++ src/model/ScoreManager.js
@@ -1,3 +1,14 @@
 class ScoreManager {
-    
+    constructor() {
+        this._coinCount = 0
+    }
+
+    get count() {
+        return this._coinCount;
+    }
+
+
+    set coinCount(value) {
+        this._coinCount = value;
+    }
 }
