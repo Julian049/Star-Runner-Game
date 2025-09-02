ff4445ad6f978b2dd302e9f44c11f01b5fa17a33
=William <ruyk1120@gmail.com>
2025-09-02 14:41:54 -0500

    Refactorizacion

--- src/view/GameScene.js
+++ src/view/GameScene.js
@@ -1,9 +1,6 @@
-import {FLOOR_HEIGHT, GRAVITY} from "../model/Config.js";
-import {Floor} from "../model/Floor.js"
-import {Arrow} from "../model/Arrow.js";
-import {Obstacle} from "../model/Obstacle.js";
-import {Coin} from "../model/Coin.js";
-import {Wall} from "../model/Wall.js";
+import { FLOOR_HEIGHT, GRAVITY } from "../model/Config.js";
+import { Wall } from "../model/Wall.js";
+import { Floor } from "../model/Floor.js"
 
 export function registerGameScene(gameFacade, k) {
     k.scene("game", () => {
@@ -13,84 +10,61 @@ export function registerGameScene(gameFacade, k) {
             gameFacade.scoreManager.reset();
         }
         gameFacade.createPlayer();
+        //Creación de jugador
         const player = gameFacade.getSelectedCharacter();
 
+        //Creación de pisos
         new Floor(0, 200, 1300, k)
         new Floor(200, 320, 1800, k)
         new Floor(0, 480, 1300, k)
         new Floor(0, k.height(), 1800, k)
-
-
         new Floor(50, 380, 50, k)
         new Floor(1400, 600, 50, k)
 
 
-        function spawnArrow() {
-            const spawnArrow = () => {
-                new Arrow(k)
-                wait(rand(1, 2), () => spawnArrow())
-            }
-            spawnArrow()
-        }
+        //Creación de obstáculos
+        
+        gameFacade.addObstacle(850, 373)
+        gameFacade.addObstacle(1200, 373)
+        gameFacade.addObstacle(500, 373)
+        gameFacade.addObstacle(700, 480)
+        gameFacade.addObstacle(400, 480)
+        gameFacade.addObstacle(1100, 480)
+        gameFacade.addObstacle(300, 200)
+        gameFacade.addObstacle(600, 200)
+        gameFacade.addObstacle(900, 200)
+        gameFacade.spawnArrow();
 
-        spawnArrow();
 
-        const addObstacle = (x, floorY) => {
-            new Obstacle(x, floorY, k)
-        }
-        addObstacle(300, 200)
-        addObstacle(600, 200)
-        addObstacle(900, 200)
-
+        //Paredes invisibles
         new Wall(0, 0, k)
         new Wall(k.width(), 0, k)
 
 
-        addObstacle(850, 373)
-        addObstacle(1200, 373)
-        addObstacle(500, 373)
-        addObstacle(700, 480)
-        addObstacle(400, 480)
-        addObstacle(1100, 480)
+        //Monedas
+        gameFacade.spawnCoin()
 
-        function spawnCoin() {
-            let x = Math.random() * (k.width() - 60) + 30
-            let y = Math.random() * (k.height() - 100) + 50
-            let coin = new Coin(x, y, k)
 
-            if (coin.sprite.isColliding("floor") ||
-                coin.sprite.isColliding("obstacle") ||
-                coin.sprite.isColliding("wall")) {
-                k.destroy(coin.sprite)
-                coin = null
-            }
-            k.wait(1, () => spawnCoin())
-        }
-
-        spawnCoin()
-
-        function endGameAndGoLose() {
-            gameFacade.endGame();
-            go("lose");
-        }
-
+        //Colisiones
         player.sprite.onCollide("arrow", () => {
             addKaboom(player.sprite.pos);
             shake();
-            endGameAndGoLose();
+            gameFacade.endGameAndGoLose();
         });
 
         player.sprite.onCollide("obstacle", () => {
             addKaboom(player.sprite.pos);
             shake();
-            endGameAndGoLose();
+            gameFacade.endGameAndGoLose();
         });
 
         player.sprite.onCollide("mapCoin", (mapCoin) => {
-            k.destroy(mapCoin)
+            k.destroy(mapCoin);
             gameFacade.addCoin();
         });
 
+        
+        //Puntajes y avisos
         const scoreLabel = k.add([
             k.text("Puntaje: " + gameFacade.getScore()),
             k.pos(k.width() - 300, 24),
