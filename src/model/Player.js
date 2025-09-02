fcba7840218dee50634f1de9eaaa287514316c15
jagd3 <jagd334@gmail.com>
2025-09-02 07:18:48 -0500

    Create Character and create scene character Select

--- src/model/Player.js
+++ src/model/Player.js
@@ -1,24 +1,29 @@
 import { JUMP_FORCE } from "./Config.js";
 export class Player {
-    constructor(k) {
+
+    nameSprite = "";
+
+    constructor(k, nameSprite) {
         this.k = k;
+        this.nameSprite = nameSprite;
         this.sprite = this.k.add([
-            this.k.sprite("player"),
+            this.k.sprite(this.nameSprite),
             this.k.pos(100, 100),
             this.k.area(),
-            this.k.body()
+            this.k.body(),
+            this.k.scale(0.2),
         ]);
     }
 
     moveLeft() {
         this.k.onKeyDown("left", () => {
-            this.sprite.move(-200, 0);   // 👈 ojo: izquierda es negativo
+            this.sprite.move(-200, 0);
         });
     }
 
     moveRight() {
         this.k.onKeyDown("right", () => {
-            this.sprite.move(200, 0);    // 👈 derecha es positivo
+            this.sprite.move(200, 0);
         });
     }
 
@@ -29,12 +34,12 @@ export class Player {
             }
         });
     }
-}
 
-export function createPlayer(k) {
-    const player = new Player(k);
-    player.moveLeft();
-    player.moveRight();
-    player.jump();
-    return player.sprite;
+    getNameSprite() {
+        return this.nameSprite;
+    }
+
+    setNameSprite(nameSprite) {
+        this.nameSprite = nameSprite;
+    }
 }
