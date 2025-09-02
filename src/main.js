964ad000a1a5a24867812b385bfff71706842f9d
jagd3 <jagd334@gmail.com>
2025-09-02 07:20:14 -0500

    Create Character and create scene character Select

--- src/main.js
+++ src/main.js
@@ -2,16 +2,26 @@ import kaboom from "kaboom"
 import { loadAssets } from "./persistence/assets.js"
 import { registerGameScene } from "./view/GameScene.js"
 import { registerLoseScene } from "./view/LoseScene.js"
+import { characterSelect } from "./view/CharacterSelect.js";
+import { GameFacade } from "./model/GameFacade.js";
+import { ScoreManager } from "./model/ScoreManager.js";
+import { LocalStoragePersistence } from "./persistence/LocalStoragePersistence.js";
 
 // Inicializamos kaboom
 const k = kaboom({
     background: [0, 0, 0],
 })
 
+const scoreManager = new ScoreManager();
+const persistence = new LocalStoragePersistence();
+const facade = new GameFacade(k, scoreManager, persistence);
+
 function startGame() {
     loadAssets(k)  
-    registerGameScene(k)    
-    registerLoseScene(k)
-    k.go("game")         
+    registerGameScene(facade, k)
+    registerLoseScene(facade, k)
+    characterSelect(facade, k)
+    k.go("characterSelect")
 }
+
 startGame()
